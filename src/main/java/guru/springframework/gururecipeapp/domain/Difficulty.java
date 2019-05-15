8f34ef1a542952c2c5a8c17d8d0515facee3b0f8
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-15 21:32:09 +0200

    JPA Enumerations

---
+++ src/main/java/guru/springframework/gururecipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package guru.springframework.gururecipeapp.domain;
+
+public enum Difficulty {
+	EASY, MODERATE, HARD
+}
--- src/main/java/guru/springframework/gururecipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/gururecipeapp/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,7 +19,7 @@ public class Recipe {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String description;
 	private Integer prepTime;
 	private Integer cookTime;
@@ -25,19 +27,19 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	// todo add
-//	private Difficulty difficulty;
 	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
+
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
-	
+
 	@Lob
 	private Byte[] image;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 
-	
 	public Long getId() {
 		return id;
 	}
@@ -116,6 +118,22 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
