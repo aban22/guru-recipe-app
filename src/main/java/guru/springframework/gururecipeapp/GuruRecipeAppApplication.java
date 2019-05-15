13939700419e8aa82152dcbef7b18ca05422994c
Abel Fernandez <abelfergodevelop@gmail.com>
2019-05-15 16:40:39 +0200

    Create the project

---
+++ src/main/java/guru/springframework/gururecipeapp/GuruRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.gururecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/gururecipeapp/GuruRecipeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.gururecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuruRecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
